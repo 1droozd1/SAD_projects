c698b5191908d1b20244d075f17689e37ef19b25
1droozd1 <dubr0vin-dk@mail.ru>
2023-06-27 21:24:02 +0300

    add new LemonadeApp

---
+++ Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -0,0 +1,135 @@
+package com.example.lemonade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lemonade.ui.theme.LemonadeTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LemonadeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LemonadeApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LemonadeApp() {
+    LemonadePicturesActivity()
+}
+
+@Composable
+private fun LemonadePicturesActivity() {
+    var result by remember { mutableStateOf(1) }
+
+    if (result > 4) {result = 1}
+
+    val imageResource = when(result) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        4 -> R.drawable.lemon_restart
+        else -> R.drawable.lemon_tree
+    }
+    val myStringResource = when(result) {
+        1 -> R.string.lemon_tree
+        2 ->  R.string.lemon
+        3 -> R.string.glass_of_lemonade
+        4 -> R.string.empty_glass
+        else -> R.string.lemon_tree
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color(0xFFffff00))
+                .fillMaxWidth()
+                .padding(10.dp),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Text(
+                text = "Lemonade",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        }
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Button(
+                onClick = {result += 1},
+                shape = MaterialTheme.shapes.extraLarge,
+                colors = ButtonDefaults.buttonColors(Color(0xFFb2f6aa)),
+                contentPadding = PaddingValues(20.dp)
+            ) {
+                Image(
+                    painter = painterResource(imageResource),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(230.dp)
+                )
+            }
+            Spacer(modifier = Modifier.height(18.dp))
+            Text(
+                text = stringResource(myStringResource),
+                fontSize = 20.sp
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LemonadeTheme {
+        LemonadeApp()
+    }
+}
